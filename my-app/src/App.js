7727e03816245d3bc95b0b315ca77b778cb4be77
vitalyvitmens <otm_@tut.by>
2023-06-14 14:26:13 +0300

    add data in project

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import styles from './app.module.css'
 
 //! useEffect
@@ -329,45 +329,108 @@ import styles from './app.module.css'
 // }
 
 //! Пользовательские хуки. Добавление товара
-import {
-	useRequestGetProducts,
-	useRequestAddVacuumCleaner,
-	useRequestUpdateSmartphone,
-	useRequestDeleteHairDryer,
-} from './hooks'
-import { ProductList, Loader } from './components'
+// import {
+// 	useRequestGetProducts,
+// 	useRequestAddVacuumCleaner,
+// 	useRequestUpdateSmartphone,
+// 	useRequestDeleteHairDryer,
+// } from './hooks'
+// import { ProductList, Loader } from './components'
 
+// export const App = () => {
+// 	const [refreshProducts, setRefreshProducts] = useState(false)
+// 	const { isLoading } = useRequestGetProducts(refreshProducts)
+
+// 	const { requestAddVacuumCleaner, isCreating } = useRequestAddVacuumCleaner(
+// 		refreshProducts,
+// 		setRefreshProducts
+// 	)
+
+// 	const { requestUpdateSmartphone, isUpdating } = useRequestUpdateSmartphone(
+// 		refreshProducts,
+// 		setRefreshProducts
+// 	)
+
+// 	const { requestDeleteHairDryer, isDeleting } = useRequestDeleteHairDryer(
+// 		refreshProducts,
+// 		setRefreshProducts
+// 	)
+
+// 	return (
+// 		<div className={styles.app}>
+// 			{isLoading ? <Loader /> : <ProductList />}
+// 			<button disabled={isCreating} onClick={requestAddVacuumCleaner}>
+// 				Добавить пылесос
+// 			</button>
+// 			<button disabled={isUpdating} onClick={requestUpdateSmartphone}>
+// 				Обновить смартфон
+// 			</button>
+// 			<button disabled={isDeleting} onClick={requestDeleteHairDryer}>
+// 				Удалить фен
+// 			</button>
+// 		</div>
+// 	)
+// }
+
+//! JSON Placeholder
+// Бывает так, что нужно протестировать вид страницы с заполнением данными, причём с нормальным количеством данных, чтобы было более наглядно и похоже на конечный вид приложения. Для этого можно использовать тот же JSON Server в связке с другим пакетом, например Faker. Таких пакетов тоже немало, они предоставляют заготовленные данные — тексты, картинки и может что-то ещё. Либо генерируют случайные данные в любых количествах.
+// Но есть сервисы, которые ещё больше упрощают тестирование. Они предлагают просто адрес ручки (эндпоинта), по которой мы можем ходить и получать массивы данных для тестирования. И не приходится даже устанавливать никакие пакеты. Посмотрим как пользоваться таким сервисом на примере JSONPlaceholder.
 export const App = () => {
-	const [refreshProducts, setRefreshProducts] = useState(false)
-	const { isLoading } = useRequestGetProducts(refreshProducts)
+	const [products, setProducts] = useState([])
+	const [isLoading, setIsLoading] = useState(false)
+	const [isCreating, setIsCreating] = useState(false)
 
-	const { requestAddVacuumCleaner, isCreating } = useRequestAddVacuumCleaner(
-		refreshProducts,
-		setRefreshProducts
-	)
+	useEffect(() => {
+		setIsLoading(true)
 
-	const { requestUpdateSmartphone, isUpdating } = useRequestUpdateSmartphone(
-		refreshProducts,
-		setRefreshProducts
-	)
+		fetch('https://jsonplaceholder.typicode.com/posts')
+			.then((loadedData) => loadedData.json())
+			.then((loadedProducts) => {
+				setProducts(loadedProducts)
+			})
+			.finally(() => setIsLoading(false))
+	}, [])
 
-	const { requestDeleteHairDryer, isDeleting } = useRequestDeleteHairDryer(
-		refreshProducts,
-		setRefreshProducts
-	)
+	const requestAddPost = () => {
+		setIsCreating(true)
+
+		fetch('https://jsonplaceholder.typicode.com/posts', {
+			method: 'POST',
+			headers: { 'Content-Type': 'application/json;charset=utf-8' },
+			body: JSON.stringify({
+				title: 'foo',
+				body: 'bar',
+				userId: 1,
+			}),
+		})
+			.then((rawResponse) => rawResponse.json())
+			.then((response) => {
+				setProducts([...products, response])
+				console.log('Статья добавлена, ответ сервера: ', response)
+			})
+			.finally(() => setIsCreating(false))
+	}
 
 	return (
 		<div className={styles.app}>
-			{isLoading ? <Loader /> : <ProductList />}
-			<button disabled={isCreating} onClick={requestAddVacuumCleaner}>
-				Добавить пылесос
-			</button>
-			<button disabled={isUpdating} onClick={requestUpdateSmartphone}>
-				Обновить смартфон
-			</button>
-			<button disabled={isDeleting} onClick={requestDeleteHairDryer}>
-				Удалить фен
+			{isLoading ? (
+				<div className="loader"></div>
+			) : (
+				products.map(({ id, title, body }) => (
+					<div key={id} style={{ border: '1px solid blue', backgroundColor: 'azure', color: 'brown', padding: '10px', margin: '10px 50px' }}>
+						{title} - {body}
+					</div>
+				))
+			)}
+			<button disabled={isCreating} onClick={requestAddPost}>
+				Добавить статью
 			</button>
 		</div>
 	)
 }
+// Как понимаете, это уже не совсем аналог сервера. Это просто ручка, которая возвращает какой-то набор данных. При создании, обновлении и удалении ручка возвращает ожидаемые данные, будто бы сервер действительно что-то делал. Но по факту мы никак не влияем на серверные данные. И чтобы изобразить работу CRUD — нам нужно реализовывать его самостоятельно с использованием состояния компонента.
+// Иногда может пригодится, но, на мой взгляд, сервис не особо полезный. Когда разрабатываете приложение, то нам обычно требуются не какие-то там данные, а такие, которые свойственны именно для нашего приложения. Однако, для таких случаев как набор статей или комментариев JSON Placeholder может помочь.
+// Промежуточные итоги:
+// JSONPlaceholder — это бесплатный онлайн-сервис, предоставляющий фейковое REST API для тестирования и прототипирования.
+// Он предоставляет набор готовых ресурсов (например, пользователей, постов, комментариев).
+// Сервис JSON Placeholder позволяет отправлять фейковые запросы на создание, обновление и удаление данных, но фактические данные на сервере не изменяются. Вместо этого, сервис возвращает фиктивные ответы, указывающие на успешное выполнение операции.
